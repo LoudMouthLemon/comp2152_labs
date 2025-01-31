8450171cd194c50af2d9492a81395662b1214b77
LoudMouthLemon <jerrylee.somera2005@gmail.com>
2025-01-31 15:42:08 -0500

    Q4

--- lab_week4.py
+++ lab_week4.py
@@ -81,6 +81,9 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+#04
+belt = []
+
 # 01 - defining the monster power
 monster_power = {
     "Fire Magic": 2,
